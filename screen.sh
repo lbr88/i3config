33909eaeb1f6698708d6651512f80d5b64b56f32
Lars Bo Rasmussen <lbr@netgroup.dk>
2017-11-16 05:08:28 +0100

    initial config for nglaptop

--- screen.sh
+++ screen.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #primary=CRT2
 #secondary=DFP2
-primary="DVI-0"
-secondary="HDMI-0"
+
+secondary="DP-2-1"
+primary="eDP-1"
 xrandr --output $primary --auto --right-of $secondary
 xrandr --output $primary --primary
